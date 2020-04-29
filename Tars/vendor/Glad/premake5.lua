6ce5829953ec5f9cef735b5fd265f92a4d93193d
RenierChaves <email@renier.com.br>
2020-04-29 10:53:17 -0300

    glad premake file update.

--- Tars/vendor/Glad/premake5.lua
+++ Tars/vendor/Glad/premake5.lua
@@ -1,24 +1,25 @@
 project "Glad"
-	kind "StaticLib"
-	language "C"
+    kind "StaticLib"
+    language "C"
+    
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    files
+    {
+        "include/glad/glad.h",
+        "include/KHR/khrplatform.h",
+        "src/glad.c"
+    }
 
-	files
+	includedirs
 	{
-		"include/glad/glad.h",
-		"include/KHR/khrplatform.h",
-		"src/glad.c"
-	}
-
-	includedirs {
 		"include"
 	}
-
-	filter "system:windows"
-		systemversion "latest"
-		staticruntime "On"
-
-	filter {"system:windows", "configurations:Release"}
-		buildoptions "/MT"
+    
+    filter "system:windows"
+        systemversion "latest"
+        staticruntime "On"
+        
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
